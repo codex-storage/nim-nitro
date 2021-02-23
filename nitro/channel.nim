61df75b1a12afb0257318c61e3865d15a6c3f921
Mark Spanbroek <mark@spanbroek.net>
2021-02-23 11:34:09 +0100

    Fix ABI encoding of channel; should be in a tuple

--- nitro/channel.nim
+++ nitro/channel.nim
@@ -12,7 +12,9 @@ type
 
 proc getChannelId*(channel: Channel): array[32, byte] =
   var writer: AbiWriter
+  writer.startTuple()
   writer.write(channel.chainId)
   writer.write(channel.participants)
   writer.write(channel.nonce)
+  writer.finishTuple()
   keccak256.digest(writer.finish()).data
--- tests/nitro/testChannel.nim
+++ tests/nitro/testChannel.nim
@@ -10,9 +10,11 @@ suite "channel":
 
   test "calculates channel id":
     var writer: AbiWriter
+    writer.startTuple()
     writer.write(channel.chainId)
     writer.write(channel.participants)
     writer.write(channel.nonce)
+    writer.finishTuple()
     let encoded = writer.finish()
     let hashed = keccak256.digest(encoded).data
     check getChannelId(channel) == hashed
