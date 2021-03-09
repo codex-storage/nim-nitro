11a2329bf26331221a5c517c3fab6f57b9986a6e
Mark Spanbroek <mark@spanbroek.net>
2021-03-09 16:50:08 +0100

    Print EthAddress as hex, add zero address

--- nitro/ethaddress.nim
+++ nitro/ethaddress.nim
@@ -5,8 +5,14 @@ export questionable
 
 type EthAddress* = distinct array[20, byte]
 
+proc zero*(_: type EthAddress): EthAddress =
+  EthAddress.default
+
 proc toArray*(address: EthAddress): array[20, byte] =
   array[20, byte](address)
+
+proc `$`*(a: EthAddress): string =
+  a.toArray().toHex()
 
 proc parse*(_: type EthAddress, hex: string): ?EthAddress =
   EthAddress(array[20, byte].fromHex(hex)).catch.toOption
